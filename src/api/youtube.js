db10210dd4cbc540f466d9e47333f639380a94f8
Charles Hamilton <crhamiltonj@gmail.com>
2019-02-16 16:51:36 -0500

    Completed app.

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAB_ccEQNF-aGtcmRuZCqzP1-fdgAgg4cc';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
+
